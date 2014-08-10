0442534fd1f11fc72e58374b85f928cfc1e8255b
Lucas Murray <lmurray@undefinedfire.com>
2014-08-10 14:51:27 +0800

    Incremented version number to v1.1.0-alpha1

--- MishiraApp/constants.h
+++ MishiraApp/constants.h
@@ -27,12 +27,13 @@
 // NOTE: Don't forget to update the values in the resource files as well
 // ("Mishira.rc" and "MishiraApp.rc"). When limited to numerical values in the
 // format "X.Y.Z.W" then "X", "Y" and "Z" match the version number below and
-// "W" begins at "0" for the first release and is incremented for every public
-// release without skipping any numbers. E.g. "-rc1" is "0", "-rc2" is "1" and
-// final is "2". If there is no pre-releases then the final would be "0".
+// "W" begins at "0" for the first release and is incremented for every PUBLIC
+// release without skipping any numbers. E.g. "-alpha1" is "0", "-rc1" is "0",
+// "-rc2" is "1" and final is "2". If there is no PUBLIC pre-releases then the
+// final would be "0".
 #define ORG_NAME "Mishira"
 #define APP_NAME "Mishira"
-#define APP_VER_STR "1.0.0-rc1"
+#define APP_VER_STR "1.1.0-alpha1"
 
 // Set this define to `1` in order to enable the automatic updater
 #define IS_UPDATER_ENABLED 0
